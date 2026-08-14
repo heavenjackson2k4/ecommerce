89d2ee81029f63497040123349b73af1e1cf3ad5
nguyennhathaicomputer <nguyennhathaigithub@gmail.com>
2026-08-14 21:09:45 +0700

    fix bug logout 419

--- laravel/app/Http/Controllers/AuthController.php
+++ laravel/app/Http/Controllers/AuthController.php
@@ -100,7 +100,7 @@ class AuthController extends Controller
         Auth::logout();
         $request->session()->invalidate();
         $request->session()->regenerateToken();
-        return redirect('/login')->with('success', 'You have been logged out.');
+        return redirect('/')->with('success', 'You have been logged out.');
     }
 
     /**
